47c1f68e2a9dd36d4d9bee677f2b3bd3ee1f19fb
Fernanda Ihjaz <166780736+fernanda-ihjaz@users.noreply.github.com>
2024-10-30 14:49:05 -0300

    🎉 Commit inicial

---
+++ Program.vb
@@ -0,0 +1,14 @@
+﻿Imports System
+
+Module Program  ' Classe Program
+
+    Sub Main(args As String())   ' Metodo Main da classe program que usa uma string como argumento. O método que é chamado automaticamente pelo runtime quando ele inicia o aplicativo
+        Console.WriteLine("Informe seu nome:")                     ' Quaisquer argumentos de linha de comando fornecidos quando o aplicativo for iniciado estão disponíveis na matriz args
+        Dim name = Console.ReadLine()
+        Dim currentDate = DateTime.Now
+        Console.WriteLine($"{vbCrLf}Olá, {name}, agora é {currentDate:d} às {currentDate:t}")
+        Console.Write($"{vbCrLf}Pressione qualquer tecla para sair...")     ' vbCrLf quebra linha
+        Console.ReadKey(True)
+    End Sub
+
+End Module
